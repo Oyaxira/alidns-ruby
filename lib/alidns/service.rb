152c0595a99d32aefa8d6f04b4929a09ba2c6807
292252585 <292252585@qq.com>
2016-09-14 17:16:57 +0800

    尝试修正 nonce was used already导致的报错

--- lib/alidns/service.rb
+++ lib/alidns/service.rb
@@ -17,41 +17,56 @@ module Alidns
     end
 
     def describe_domains
-      params = public_params
-      params["Action"] = "DescribeDomains"
-      params = params_to_str(params)
-      params = params + "&" + Alidns::Sign.sign("GET", @app_key, @app_secret, params)
-      url = "#{Alidns.config.host}/?#{params}"
-      response = RestClient.get(url)
-      response.body
+      begin
+        retries ||= 0
+        params = public_params
+        params["Action"] = "DescribeDomains"
+        params = params_to_str(params)
+        params = params + "&" + Alidns::Sign.sign("GET", @app_key, @app_secret, params)
+        url = "#{Alidns.config.host}/?#{params}"
+        response = RestClient.get(url)
+        response.body
+      rescue
+        retry if (retries += 1) < 3
+      end
     end
 
     def describe_doname_record(domainname)
-      params = public_params
-      params["Action"] = "DescribeDomainRecords"
-      params["DomainName"] = "#{domainname}"
-      params = params_to_str(params)
-      params = params + "&" + Alidns::Sign.sign("GET", @app_key, @app_secret, params)
-      url = "#{Alidns.config.host}/?#{params}"
-      response = RestClient.get(url)
-      response.body
+      begin
+        retries ||= 0
+        params = public_params
+        params["Action"] = "DescribeDomainRecords"
+        params["DomainName"] = "#{domainname}"
+        params = params_to_str(params)
+        params = params + "&" + Alidns::Sign.sign("GET", @app_key, @app_secret, params)
+        url = "#{Alidns.config.host}/?#{params}"
+        response = RestClient.get(url)
+        response.body
+      rescue
+        retry if (retries += 1) < 3
+      end
     end
 
     def update_domain_record(record_id, rr , type, value, ttl=600 , priority = 1, line="default")
-      params = public_params
-      params["Action"] = "UpdateDomainRecord"
-      params["RecordId"] = record_id
-      params["RR"] = rr
-      params["Type"] = type
-      params["Value"] = value
-      params["TTL"] = ttl
-      params["Priority"] = priority
-      params["Line"] = line
-      params = params_to_str(params)
-      params = params + "&" + Alidns::Sign.sign("GET", @app_key, @app_secret, params)
-      url = "#{Alidns.config.host}/?#{params}"
-      response = RestClient.get(url)
-      response.body
+      begin
+        retries ||= 0
+        params = public_params
+        params["Action"] = "UpdateDomainRecord"
+        params["RecordId"] = record_id
+        params["RR"] = rr
+        params["Type"] = type
+        params["Value"] = value
+        params["TTL"] = ttl
+        params["Priority"] = priority
+        params["Line"] = line
+        params = params_to_str(params)
+        params = params + "&" + Alidns::Sign.sign("GET", @app_key, @app_secret, params)
+        url = "#{Alidns.config.host}/?#{params}"
+        response = RestClient.get(url)
+        response.body
+      rescue
+        retry if (retries += 1) < 3
+      end
     end
 
     def public_params
@@ -61,10 +76,11 @@ module Alidns
       params["SignatureMethod"] = "HMAC-SHA1"
       params["Timestamp"] = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
       params["SignatureVersion"] = "1.0"
-      params["SignatureNonce"]= rand(999999999999999999).to_s
+      params["SignatureNonce"]= rand(10 ** 30).to_s.rjust(30,'0')
       params["AccessKeyId"] = Alidns.config.app_key
       params
     end
+
 
     def params_to_str(params)
       params = params.to_a.map{|k| "#{k.first}=#{k.last}"}.join('&')
